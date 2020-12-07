a5fa7b2c8651284a1c2e92dc8b1459c2ad50bd10
Ethan P <46476926+EthanPlusPlus@users.noreply.github.com>
2020-12-07 23:12:34 +0200

    shape movement
    basic forward movement

---
+++ Shape Shuffle/Assets/Scripts/ShapeMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShapeMovement : MonoBehaviour
+{
+    [SerializeField] float speed = 1;
+    public float xVec, yVec;
+
+    Rigidbody shpR;
+
+    void Start()
+    {
+       shpR = GetComponent<Rigidbody>();
+
+       Move();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void Move()
+    {
+        xVec = Mathf.Sin(1.308997f);             //75 * (Mathf.PI/180)
+        yVec = Mathf.Cos(1.308997f);             //75 * (Mathf.PI/180)
+
+        shpR.AddForce(xVec * speed, -yVec * speed, 0);
+    }
+}
