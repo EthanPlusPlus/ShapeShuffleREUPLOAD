e8848a87bed032a46aca2eca43ef67ab1493c9d3
Ethan P <46476926+EthanPlusPlus@users.noreply.github.com>
2020-12-08 14:05:50 +0200

    shuffle working
    key press input and swipe input
    made road longer
    added more lights

--- Shape Shuffle/Assets/Scripts/ShapeMovement.cs
+++ Shape Shuffle/Assets/Scripts/ShapeMovement.cs
@@ -7,18 +7,28 @@ public class ShapeMovement : MonoBehaviour
     [SerializeField] float speed = 1;
     public float xVec, yVec;
 
+    public Vector3 mousePos;
+    public Vector3 mouseStartClick;
+    public Vector2 mouseTempPos;
+
+    public bool clicked, leftSwiped, rightSwiped;
+
+    public GameObject mouseTemp;
+
     Rigidbody shpR;
+    Transform shpT;
 
     void Start()
     {
        shpR = GetComponent<Rigidbody>();
+       shpT = GetComponent<Transform>();
 
        Move();
     }
 
     void Update()
     {
-        
+        StartCoroutine(Shuffle());
     }
 
     void Move()
@@ -27,5 +37,59 @@ public class ShapeMovement : MonoBehaviour
         yVec = Mathf.Cos(1.308997f);             //75 * (Mathf.PI/180)
 
         shpR.AddForce(xVec * speed, -yVec * speed, 0);
+    }
+
+    IEnumerator Shuffle()
+    {
+        //mouse swipe input
+
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mouseTemp.transform.position = 10 * new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+        mouseTempPos = mouseTemp.transform.position;    
+
+        Vector3 shapePos = transform.position;
+
+        if(Input.GetMouseButtonDown(0)){
+            
+                mouseStartClick = mouseTempPos;
+
+                yield return new WaitForSeconds(0.1f);
+
+                if(mouseTempPos.x > 0){
+                    if(shapePos.z > -1.75f){
+                        rightSwiped = true;
+                    }
+                }else if(mouseTempPos.x < 0){
+                    if(shapePos.z < 1.75f){
+                        leftSwiped = true;
+                    }
+                }
+        }
+
+        if(leftSwiped){
+            shapePos.z = shapePos.z + 1.75f;
+            transform.position = shapePos;
+            leftSwiped = false;
+        }
+        
+        if(rightSwiped){
+            shapePos.z = shapePos.z - 1.75f;
+            transform.position = shapePos;
+            rightSwiped = false;
+        }
+
+        //key press input
+
+        if(Input.GetKeyDown(KeyCode.LeftArrow)){
+            if(shapePos.z < 1.75f){
+                shapePos.z = shapePos.z + 1.75f;
+                transform.position = shapePos;
+            }
+        }else if(Input.GetKeyDown(KeyCode.RightArrow)){
+            if(shapePos.z > -1.75f){
+                shapePos.z = shapePos.z - 1.75f;
+                transform.position = shapePos;
+            }
+        }
     }
 }
